9f1e35630d05b92cb7d7df98d1895e10a4a9a69e
dkelly0226 <147778458+dkelly0226@users.noreply.github.com>
2025-12-16 12:35:49 -0800

    Add files via upload

---
+++ Website/learn.js
@@ -0,0 +1,10 @@
+console.log('Happy developing ✨')
+
+// let is the keyword for storing variables
+let message= 'Hello World!';
+
+console.log(message)
+
+// const stores unchanging variables
+const myBirthday = '02-26-2001';
+console.log(myBirthday);
